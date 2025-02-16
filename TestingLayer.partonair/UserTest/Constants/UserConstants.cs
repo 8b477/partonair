86be4b6fad93e9f6aec248de936f6318ed39efde
8b477 <jonathan_buchet@outlook.fr>
2025-02-16 14:51:21 +0100

    Rename-constants : Full uppercase for match with the standard

--- TestingLayer.partonair/UserTest/Constants/UserConstants.cs
+++ TestingLayer.partonair/UserTest/Constants/UserConstants.cs
@@ -4,9 +4,9 @@ namespace TestingLayer.partonair.UserTest.Constants
 {
     internal static class UserConstants
     {
-        public const string Name = "jhon";
-        public const string Email = "jhon@mail.be";
-        public const string Password = "Test1234*";
-        public const string PasswordHashed = "hashedPassword123*";
+        public const string NAME = "jhon";
+        public const string EMAIL = "jhon@mail.be";
+        public const string PASSWORD = "Test1234*";
+        public const string PASSWORD_HASHED = "hashedPassword123*";
     }
 }
