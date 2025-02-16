bc3387808372a87c9071fb33995eba8273d36635
8b477 <jonathan_buchet@outlook.fr>
2025-02-16 15:58:51 +0100

    Create-test: DeleteUserCommandHandler, success or no and propagation errors

---
+++ TestingLayer.partonair/UserTest/Commands/DeleteUserCommandHandlerTest.cs
@@ -0,0 +1,56 @@
+﻿using ApplicationLayer.partonair.MediatR.Commands.Users;
+
+using DomainLayer.partonair.Exceptions.Enums;
+using DomainLayer.partonair.Exceptions;
+
+using Moq;
+
+using TestingLayer.partonair.UserTest.Abstracts;
+
+
+namespace TestingLayer.partonair.UserTest.Commands
+{
+    public class DeleteUserCommandHandlerTest : UserBaseClassTest
+    {
+        private readonly DeleteUserCommandHandler _handler;
+
+        public DeleteUserCommandHandlerTest() : base()
+        {
+            _handler = new DeleteUserCommandHandler(_mockUserService.Object);
+        }
+
+
+        [Fact]
+        public async Task DeleteUserCommandHandler_ShouldCallDeleteService()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            _mockUserService.Setup(s => s.DeleteService(It.Is<Guid>(g => g != Guid.Empty)))
+                .Verifiable();
+
+            // Act
+            await _handler.Handle(new DeleteUserCommand(userId), CancellationToken.None);
+
+            // Assert
+            _mockUserService.Verify(s => s.DeleteService(userId), Times.Once());
+        }
+
+        [Fact]
+        public async Task DeleteUserCommandHandler_WhenOperationCanceled_ShouldThrowInfrastructureLayerException()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            _mockUserService.Setup(s => s.DeleteService(It.IsAny<Guid>()))
+                .ThrowsAsync(new OperationCanceledException("Operation was canceled."));
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<InfrastructureLayerException>(async () =>
+                await _handler.Handle(new DeleteUserCommand(userId), CancellationToken.None));
+
+            Assert.Equal(InfrastructureLayerErrorType.CancelationDatabaseException, exception.ErrorType);
+            Assert.Contains("Operation was canceled.", exception.Message);
+        }
+
+
+    }
+}
