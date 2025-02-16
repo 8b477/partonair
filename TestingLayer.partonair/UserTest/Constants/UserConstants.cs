214c242985ac769c56e3e9ea1a8fcab9ddc815d2
8b477 <jonathan_buchet@outlook.fr>
2025-02-16 16:42:20 +0100

    Create-test : UpdateUserCommandHandler, insertion and propagation errors

--- TestingLayer.partonair/UserTest/Constants/UserConstants.cs
+++ TestingLayer.partonair/UserTest/Constants/UserConstants.cs
@@ -7,6 +7,8 @@ namespace TestingLayer.partonair.UserTest.Constants
         public const string NAME = "jhon";
         public const string EMAIL = "jhon@mail.be";
         public const string PASSWORD = "Test1234*";
+        public const string OLD_PASSWORD = "Test1234*";
+        public const string NEW_PASSWORD = "Test12345*";
         public const string PASSWORD_HASHED = "hashedPassword123*";
         public const string ROLE_VISITOR = "Visitor";
         public const string ROLE_EMPLOYEE = "Employee";
