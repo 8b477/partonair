df312c67dcf3ed4923a3bc94058ffdd8004950c3
8b477 <jonathan_buchet@outlook.fr>
2025-02-17 11:24:49 +0100

    Create-test : GetByNameUserQueryHandler, succes insertion and propagation error

---
+++ TestingLayer.partonair/UserTest/Queries/GetByNameUserQueryHandlerTest.cs
@@ -0,0 +1,59 @@
+﻿using ApplicationLayer.partonair.DTOs;
+using ApplicationLayer.partonair.MediatR.Queries.Users;
+
+using DomainLayer.partonair.Exceptions;
+using DomainLayer.partonair.Exceptions.Enums;
+
+using Moq;
+
+using TestingLayer.partonair.UserTest.Abstracts;
+using TestingLayer.partonair.UserTest.Constants;
+
+namespace TestingLayer.partonair.UserTest.Queries
+{
+    public class GetByNameUserQueryHandlerTest : UserBaseClassTest
+    {
+        private readonly GetByNameUserQueryHandler _handler;
+        private readonly ICollection<UserViewDTO> _usersList;
+
+        public GetByNameUserQueryHandlerTest():base()
+        {
+            _handler = new GetByNameUserQueryHandler(_mockUserService.Object);
+            _usersList =
+[
+new (Guid.NewGuid(), UserConstants.NAME,UserConstants.EMAIL,false,DateTime.Now,DateTime.Now,UserConstants.ROLE_VISITOR,null),
+                new (Guid.NewGuid(), UserConstants.NAME,UserConstants.EMAIL,false,DateTime.Now,DateTime.Now,UserConstants.ROLE_VISITOR,null),
+            ];
+        }
+
+
+        [Fact]
+        public async Task GetByNameUserQueryHandler_ShouldReturn_UsersList()
+        {
+            _mockUserService.Setup(s => s.GetByNameAsyncService(UserConstants.NAME)).ReturnsAsync(_usersList);
+
+            var result = await _handler.Handle(new GetByNameUserQuery(UserConstants.NAME), CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.IsType<List<UserViewDTO>>(result);
+
+            _mockUserService.Verify(v => v.GetByNameAsyncService(UserConstants.NAME),Times.Once);
+        }
+
+        [Fact]
+        public async Task GetByNameUserQueryHandler_ShouldThrow_ResourceNotFoundException()
+        {
+            _mockUserService.Setup(s => s.GetByNameAsyncService("notFound"))
+                .ThrowsAsync(new InfrastructureLayerException(InfrastructureLayerErrorType.ResourceNotFoundException));
+
+            var exception = await Assert.ThrowsAsync<InfrastructureLayerException>(() =>
+            _handler.Handle(new GetByNameUserQuery("notFound"), CancellationToken.None));
+
+            Assert.Equal(InfrastructureLayerErrorType.ResourceNotFoundException, exception.ErrorType);
+
+            _mockUserService.Verify(v => v.GetByNameAsyncService("notFound"), Times.Once);
+        }
+
+    }
+}
