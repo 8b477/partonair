f4c767ff67dc8d06cd5cd18e2258fcc39e60dff9
8b477 <jonathan_buchet@outlook.fr>
2025-02-17 10:36:58 +0100

    Create-test : GetAllUserQueryHandler return empty collection or fill collection and propagation CancelationDatabaseException error

---
+++ TestingLayer.partonair/UserTest/Queries/GetAllUserQueryHandlerTest.cs
@@ -0,0 +1,81 @@
+﻿using ApplicationLayer.partonair.DTOs;
+using ApplicationLayer.partonair.MediatR.Commands.Users;
+using ApplicationLayer.partonair.MediatR.Queries.Users;
+
+using DomainLayer.partonair.Exceptions.Enums;
+
+using DomainLayer.partonair.Exceptions;
+
+using Moq;
+
+using TestingLayer.partonair.UserTest.Abstracts;
+using TestingLayer.partonair.UserTest.Constants;
+
+
+namespace TestingLayer.partonair.UserTest.Queries
+{
+    public class GetAllUserQueryHandlerTest : UserBaseClassTest
+    {
+        private readonly GetAllUserQueryHandler _handler;
+        private readonly ICollection<UserViewDTO> _usersList;
+
+        public GetAllUserQueryHandlerTest() :base()
+        {
+            _handler = new GetAllUserQueryHandler(_mockUserService.Object);
+            _usersList =
+            [
+    new (Guid.NewGuid(), UserConstants.NAME,UserConstants.EMAIL,false,DateTime.Now,DateTime.Now,UserConstants.ROLE_VISITOR,null),
+                new (Guid.NewGuid(), UserConstants.NAME,UserConstants.EMAIL,false,DateTime.Now,DateTime.Now,UserConstants.ROLE_VISITOR,null),
+            ];
+        }
+
+        [Fact]
+        public async Task GetAllUserQueryHandler_ShoudlReturn_ListUserViewDTO()
+        {
+            _mockUserService.Setup(s => s.GetAllAsyncService()).ReturnsAsync(_usersList);
+
+            var result = await _handler.Handle(new GetAllUserQuery(), CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.IsType<List<UserViewDTO>>(result);
+            Assert.Equal(2, _usersList.Count);
+            Assert.Equal(_usersList, result);
+
+            _mockUserService.Verify(v => v.GetAllAsyncService(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAllUserQueryHandler_ShoudlReturn_EmptyListUserViewDTO()
+        {
+            ICollection<UserViewDTO> usersListEmpty = [];
+
+            _mockUserService.Setup(s => s.GetAllAsyncService()).ReturnsAsync(usersListEmpty);
+
+            var result = await _handler.Handle(new GetAllUserQuery(), CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.IsType<List<UserViewDTO>>(result);
+            Assert.Empty(result);
+            Assert.Equal(usersListEmpty, result);
+
+            _mockUserService.Verify(v => v.GetAllAsyncService(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAllUserQueryHandler_WhenUserCanceledOperation_ShouldThrow_InfrastructureOperationCanceledException()
+        {
+            // Arrange
+            _mockUserService.Setup(s => s.GetAllAsyncService())
+                .ThrowsAsync(new InfrastructureLayerException(InfrastructureLayerErrorType.CancelationDatabaseException));
+
+            // Act && Assert
+            var exception = await Assert.ThrowsAsync<InfrastructureLayerException>(() =>
+                _handler.Handle(new GetAllUserQuery(), CancellationToken.None));
+
+            Assert.Equal(InfrastructureLayerErrorType.CancelationDatabaseException, exception.ErrorType);
+
+            _mockUserService.Verify(v => v.GetAllAsyncService(), Times.Once);
+        }
+
+    }
+}
